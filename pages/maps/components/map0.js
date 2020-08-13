e7ad1b4e5128c212dfe66fdfdc3c43333eb4c4da
Nick Hvattum <nhvattum@gmail.com>
2020-08-13 16:01:39 -0700

    deploying

--- pages/maps/components/map0.js
+++ pages/maps/components/map0.js
@@ -4,33 +4,29 @@ import { Component } from 'react'
 import ReactMapGL, { Marker, Popup } from 'react-map-gl'
 
 
+setShow = props.setShow
+
 const places = [{
   name: "Eat and Drink",
   lat: -11.98938,
   lon: -77.08634,
-  address: "Jiron Santa 137\nentrada a Alameda Chabuca Granda Lima\n15001, Peru",
-  bannerURL: "https://i.imgur.com/7Bxx12L.png",
-  bodyURL: "https://i.imgur.com/OAZfivo.png" 
+  info: "get ur grubz"
 }, {
   name: "Basilica y Convento de San Francisco do Lima",
   lat: -12.052660,
-  lon: -77.033030,
-  address: "Jirón Lampa\nCercado de Lima\n15001, Peru"
+  lon: -77.033030
 }, {
   name: "Larco Museum",
   lat: -12.072400,
-  lon: -77.066000,
-  address: "Av. Simón Bolivar 1515\nPueblo Libre\n15084, Peru"
+  lon: -77.066000
 }, {
   name: "Park of the Reserve",
   lat: -12.036910,
-  lon: -77.091320,
-  address: "Jr. Madre de Dios S/N\nCercado de Lima, Peru"
+  lon: -77.091320
 }, {
   name: "Bus to Macchu Picchu",
   lat: -12.089730,
-  lon: -77.016160,
-  address: ""
+  lon: -77.016160
 }
 ]
 
@@ -39,37 +35,30 @@ class Map extends Component {
 
   setSelectedMarker = (object) => {
     this.setState({
-      selectedMarker: object,
-      sidebarURL: object.bodyURL
+      selectedMarker: object
     })
   };
 
   closePopup = () => {
     this.setState({
-      selectedMarker: null,
-      sidebarURL: ""
+      selectedMarker: null
     })
   }
 
   state = {
     viewport: {
-      width: 1200,
-      height: 700,
+      width: '100vw',
+      height: '100vh',
       latitude: -12.10,
       longitude: -77.06,
-      zoom: 11,
-      scrollZoom: false
+      zoom: 11
     },
-    selectedMarker: null,
-    sidebarURL: ""
+    selectedMarker: null
   };
  
   render() {
     return (
-      <div className="mapContainer">
-        <head>
-          <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,700&display=swap" rel="stylesheet"/>
-        </head>
+      <div>
         <div>
         </div>
         <ReactMapGL
@@ -92,9 +81,7 @@ class Map extends Component {
                     closeOnClick={true}
                     anchor="bottom"
                     offsetLeft={-88}
-                    ><h3>{this.state.selectedMarker.name}</h3>
-                    {/* <h3>{this.state.selectedMarker.address}</h3> */}
-                      </Popup>
+                    ><h3>{this.state.selectedMarker.name}</h3></Popup>
                     ) : 
                     null
                     }
